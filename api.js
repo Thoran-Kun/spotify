502aab1036d7f2e9751fa7ae65d2ad0bf944c204
Salvatore <pepesalvatore12@gmail.com>
2025-11-12 17:49:57 +0100

    api search

--- api.js
+++ api.js
@@ -1,17 +1,42 @@
 //https://striveschool-api.herokuapp.com/api/deezer/album/75621062
-let currentAlbum;
+let currentAlbum
 const getAlbum = async function (id) {
   await fetch(`https://striveschool-api.herokuapp.com/api/deezer/album/${id}`, {
     method: "GET",
   })
     .then((res) => {
-      if (!res.ok) throw new Error(`[${res.status}] http status not ok`);
-      return res.json();
+      if (!res.ok) throw new Error(`[${res.status}] http status not ok`)
+      return res.json()
     })
     .then((data) => {
-      currentAlbum = data;
+      currentAlbum = data
     })
     .catch((err) => {
-      console.error(`encountered error: ${err}`);
-    });
-};
+      console.error(`encountered error: ${err}`)
+    })
+}
+
+let currentResults
+
+const getResults = async function (ricerca) {
+  await fetch(
+    `https://striveschool-api.herokuapp.com/api/deezer/search?q=${ricerca}`,
+    {
+      method: "GET",
+    }
+  )
+    .then((res) => {
+      if (!res.ok) throw new Error(`[${res.status}] http status not ok`)
+      return res.json()
+    })
+    .then((data) => {
+      currentResults = data
+    })
+    .catch((err) => {
+      console.error(`encountered error: ${err}`)
+    })
+}
+
+const ResultList = function () {
+  //DA FIXARE
+}
